0c723e1e4c257eb67dab77ed99f2491f78d3cbe2
Katherine Guerrero <kfguerrero@uc.cl>
2020-07-04 22:00:47 -0400

    uncomment

--- classification.py
+++ classification.py
@@ -26,49 +26,49 @@ Xv, yv = np.array(val_data['X']), np.array(val_data['y'])
 if __name__ == "__main__":
 
     def test(Xv, yv):
-        # # Probamos con 1 hasta 10 vecinos para ver qué resulta mejor.
-        # max_knn = 0
-        # best_k = -1
-        # for k in range(1, 30):
-        #     knn = KNeighborsClassifier(n_neighbors=k, weights='uniform')
-        #     knn.fit(X, y)
-        #     score = knn.score(Xv, yv)
-        #     if score >= max_knn:
-        #         max_knn = score
-        #         best_k = k
-        # print(f'best knn score: {max_knn} with {best_k} neighbors')
+        # Probamos con 1 hasta 10 vecinos para ver qué resulta mejor.
+        max_knn = 0
+        best_k = -1
+        for k in range(1, 30):
+            knn = KNeighborsClassifier(n_neighbors=k, weights='uniform')
+            knn.fit(X, y)
+            score = knn.score(Xv, yv)
+            if score >= max_knn:
+                max_knn = score
+                best_k = k
+        print(f'best knn score: {max_knn} with {best_k} neighbors')
 
-        # best_rf = 0
-        # best_n = 0
-        # for n in range(1, 20):
-        #     rforest = RandomForestClassifier(max_depth=n, n_estimators=100)
-        #     rforest.fit(X, y)
-        #     score = rforest.score(Xv, yv)
-        #     if score >= best_rf:
-        #         best_rf = score
-        #         best_n = n
-        # print(
-        #     f'best random forest score with 100 estimators: {best_rf} with {best_n} max_depth')
+        best_rf = 0
+        best_n = 0
+        for n in range(1, 20):
+            rforest = RandomForestClassifier(max_depth=n, n_estimators=100)
+            rforest.fit(X, y)
+            score = rforest.score(Xv, yv)
+            if score >= best_rf:
+                best_rf = score
+                best_n = n
+        print(
+            f'best random forest score with 100 estimators: {best_rf} with {best_n} max_depth')
 
-        # best_est = 0
-        # for est in range(10, 100, 5):
-        #     rforest = RandomForestClassifier(max_depth=best_n, n_estimators=est)
-        #     rforest.fit(X, y)
-        #     score = rforest.score(Xv, yv)
-        #     if score >= best_rf:
-        #         best_rf = score
-        #         best_est = est
-        # print(f'best random forest score: {best_rf} with {est} estimators')
+        best_est = 0
+        for est in range(10, 100, 5):
+            rforest = RandomForestClassifier(max_depth=best_n, n_estimators=est)
+            rforest.fit(X, y)
+            score = rforest.score(Xv, yv)
+            if score >= best_rf:
+                best_rf = score
+                best_est = est
+        print(f'best random forest score: {best_rf} with {est} estimators')
 
-        # svc = make_pipeline(StandardScaler(), SVC(gamma='auto'))
-        # svc.fit(X, y)
-        # score = svc.score(Xv, yv)
-        # print(f'SVM score: {score}')
+        svc = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+        svc.fit(X, y)
+        score = svc.score(Xv, yv)
+        print(f'SVM score: {score}')
 
-        # clf = LogisticRegression(random_state=0, solver='lbfgs',
-        #                         multi_class='auto', max_iter=1000).fit(X, y)
-        # score = clf.score(Xv, yv)
-        # print(f'Logistic regression score: {score}')
+        clf = LogisticRegression(random_state=0, solver='lbfgs',
+                                multi_class='auto', max_iter=1000).fit(X, y)
+        score = clf.score(Xv, yv)
+        print(f'Logistic regression score: {score}')
     
         D = X
         T = 2    # number of non-zero coefficients
